b91da773eca52b4d885b47ee4741f6cc91bf8b75
Дмитрий Брилько <DimaBrilko@gmail.com>
2020-10-20 09:17:22 +0300

    qwe

---
+++ Script.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert').strict;
+const script = require("./Script.js");
+function Some(a, b){
+    it("some", function(){
+        assert.equal(a, b);
+    });
+}
+Some(script.a, script.a1);
+Some(script.a1, script.b);
+Some(script.a, script.b);
