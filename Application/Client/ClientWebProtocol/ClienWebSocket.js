8d6033115018754e14f3ca3bb359524f33eec667
Дмитрий Брилько <DimaBrilko@gmail.com>
2020-10-20 17:59:42 +0300

    Удаление лишних файлов и создание структуры файлов приложения

---
+++ Application/Client/ClientWebProtocol/ClienWebSocket.js
@@ -0,0 +1,5 @@
+/**
+ * Слушает сервер по протоколу WebSocket и считывает, отправляемые с него, значения. 
+ * Не предоставляет API
+ * Использует GraphicUserInterface | Display(arrayValues)
+ */
--- Script.test.js
+++
@@ -1,9 +0,0 @@
-const assert = require('assert').strict;
-const script = require("./Script.js");
-function Some(a, b){
-    it("some", function(){
-        assert.equal(a, b);
-    });
-}
-Some(script.a, script.a1);
-Some(script.a1, script.b);
