8733ec865ebfb4253cbcc4a574ffeb3e8406709e
Дмитрий Брилько <DimaBrilko@gmail.com>
2020-10-20 17:12:21 +0300

    Удаление лишних файлов

--- WebSocketServer.js
+++
@@ -1,8 +0,0 @@
-const ws = require('ws');
-
-const server = new ws.Server({port: 3000});
-
-server.on('connection', ws => {
-    ws.send(Здравствуй);
-    console.log("Подключён новый пользователь");
-});
